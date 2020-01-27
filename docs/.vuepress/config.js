7e3fda918cdd081057fe2240baac7555cecf40eb
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2020-01-27 13:40:44 +0100

    Add sidebar config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,32 @@
 module.exports = {
     title: 'FATML 2020 - Barcelona',
     description: 'Notes from the 2020 FATML conference held in Barcelona',
-    base: '/fatml-bar20/'
+    base: '/fatml-bar20/',
+    themeConfig: {
+      sidebar: [
+        '/Program',
+        {
+          title: 'Tutorials',
+          collapsible: true,
+          children:[
+            '/notes/tutorials/ai_explainability_360'
+          ]
+        },
+        {
+          title: 'CRAFTs',
+          collapsible: true,
+          children:[
+            '/notes/crafts/'
+          ]
+        },
+        {
+          title: 'Talks',
+          collapsible: true,
+          children:[
+            '/notes/talks/'
+          ]
+        }
+      ],
+      sidebarDepth: 0
+    }
   }
